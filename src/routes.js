70b22d4cf4d5204e32a3f762bba9d0aa51c4a076
matheushenrique <matheus.henriquepr@gmail.com>
2020-06-29 21:56:19 -0300

    criacao de controller para criacao de de usuario

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express';
 
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Matheus',
-    email: 'teste@exemplo',
-    password_hash: '134543',
-  });
-  res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
