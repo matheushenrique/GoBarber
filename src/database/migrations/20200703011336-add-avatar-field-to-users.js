1697e0ca33f1927277029bc31737107b9f5f1f93
matheushenrique <matheus.henriquepr@gmail.com>
2020-07-02 22:52:56 -0300

    criacao de migration e alteracao no model para adicao de coluna fk

---
+++ src/database/migrations/20200703011336-add-avatar-field-to-users.js
@@ -0,0 +1,14 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('users', 'avatar_id', {
+      type: Sequelize.INTEGER,
+      references: { model: 'files', key: 'id' },
+      onUpdate: 'CASCADE',
+      onDelete: 'SET NULL',
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.removeColumn('users', 'avatar_id');
+  },
+};
